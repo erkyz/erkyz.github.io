8d5b3d16bff33bf133594063f0383d52a7512da5
Eric <erkyz@aim.com>
2015-06-09 16:38:23 -0400

    testing CORS

--- js/thread.js
+++ js/thread.js
@@ -64,8 +64,37 @@ function showInfo(data, tabletop, n, boxes, total) {
   }, 200);
 }
 
-goodreads = require('goodreads');
-gr = new goodreads.client({ 'key': 'mg9FnpqugGOl2CmAMWH9g', 'secret': '7p1yvrCmbuEJWgYHqXbqogc3rxPRTBvMW0V3Xbx38WE' });
-gr.getShelves('33571041', function(json) {
-    console.log(json);
-});
+function createCORSRequest(method, url) {
+  var xhr = new XMLHttpRequest();
+    if ("withCredentials" in xhr) {
+
+      // Check if the XMLHttpRequest object has a "withCredentials" property.
+      // "withCredentials" only exists on XMLHTTPRequest2 objects.
+      xhr.open(method, url, true);
+
+    } else if (typeof XDomainRequest != "undefined") {
+
+      // Otherwise, check if XDomainRequest.
+      // XDomainRequest only exists in IE, and is IE's way of making CORS requests.
+       xhr = new XDomainRequest();
+       xhr.open(method, url);
+    } else {
+
+      // Otherwise, CORS is not supported by the browser.
+      xhr = null;
+    }
+  return xhr;
+}
+
+var xhr = createCORSRequest('GET', 'www.goodreads.com/shelf/list.xml?user_id=33571041&key=mg9FnpqugGOl2CmAMWH9g');
+if (!xhr) {
+  throw new Error('CORS not supported');
+}
+
+xhr.onload = function() {
+   var responseText = xhr.responseText;
+    console.log(responseText);
+     // process the response.
+}
+
+xhr.send();

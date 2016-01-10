9370fa6493866c8c269e3b17d8d3e50f3c0f10f5
Eric <erkyz@aim.com>
2016-01-10 16:25:49 -0500

    temp

--- js/general.js
+++ js/general.js
@@ -10,11 +10,14 @@ $(document).ready(function() {
   if (window.orientation == 0) {
     alert("Please rotate your device to landscape mode.");
     $('body').hide();
-    $('html').append("<div id='rotate'><img src='media/img/mobile_rotate_icon.png'></img>Please rotate your device. Make sure that auto-portrait mode is turned off.</div>");
+    $('html').append("<div id='rotate'><img src='media/img/mobile_rotate_icon.png'></img> \n Please rotate your device. Make sure that auto-portrait mode is turned off.</div>");
     $('html').css('background-color','black');
     $('#rotate').css('color','white');
     $('#rotate').css('margin','auto');
+    $('#rotate').css('vertical-align','middle');
     $('#rotate').css('width','60%');
+    $('#rotate').css('font-family',"'Playfair display', serif");
+    $('#rotate').css('font-size',"40px");
     window.addEventListener("orientationchange",function() {
       if (window.orientation != 0) { 
         $('body').show();
